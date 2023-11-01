7fab0ced22970a1ce146425fd0b5dda1b6fe5b82
Anuraag Sinha <anuraagsinha@hotmail.com>
2023-11-01 10:28:43 -0700

    fixed data format

--- src/App.jsx
+++ src/App.jsx
@@ -17,8 +17,7 @@ function Category({categoryData}) {
 
   const onDragEnd = result => {
     //console.log(result);
-    const {destination, source, draggableId} = result;
-  
+    const {destination, source} = result;
     // Dropped from list
     if (!destination) {
       return;
@@ -29,24 +28,21 @@ function Category({categoryData}) {
         return;
     }
   
-    const newItemIds = Array.from(data.itemIds)
     // Remove from old position, insert into new
-    newItemIds.splice(source.index, 1);
-    newItemIds.splice(destination.index, 0, draggableId)
-    
+    const newItems = Array.from(data.items)
+    const [item] = newItems.splice(source.index, 1);
+    newItems.splice(destination.index, 0, item)
     const newData = {
       ...data,
-      itemIds: newItemIds
+      items: newItems
     }
 
     setData(newData);
   }
 
-  const items = data.itemIds.map(itemID => data.items[itemID]);
-
   return (
     <DragDropContext onDragEnd={onDragEnd}>
-      <Column key={data.id} column={data} items={items} />;
+      <Column key={data.id} column={data} items={data.items} />;
     </DragDropContext>
   );
 }
@@ -55,7 +51,7 @@ function Category({categoryData}) {
 export default function App() {
   return (
     <Container>
-      {initialData.columnOrder.map(columnID => {
+      {Object.keys(initialData.columns).map(columnID => {
           const column = initialData.columns[columnID];
           return <Category key={columnID} categoryData={column} />;
         })}
--- src/initialData.js
+++ src/initialData.js
@@ -6,41 +6,35 @@ const initialData = {
       'column1': {
         id: 'column1',
         title: 'Movies',
-        itemIds: ['movie1', 'movie2', 'movie3', 'movie4'],
-        items: {
-          'movie1': { id: 'movie1', content: 'Intersteller' },
-          'movie2': { id: 'movie2', content: 'Inception' },
-          'movie3': { id: 'movie3', content: 'Tenent' },
-          'movie4': { id: 'movie4', content: 'Memento' },
-        },
+        items: [
+          { id: 'movie1', content: 'Intersteller' },
+          { id: 'movie2', content: 'Inception' },
+          { id: 'movie3', content: 'Tenent' },
+          { id: 'movie4', content: 'Memento' },
+        ],
       },
 
       'column2': {
         id: 'column2',
         title: 'TV Shows',
-        itemIds: ['show1', 'show2', 'show3'],
-        items: {
-          'show1': { id: 'show1', content: 'How I Met Your Mother' },
-          'show2': { id: 'show2', content: 'Attack on Titan' },
-          'show3': { id: 'show3', content: 'Steins Gate' },
-        },
+        items: [
+          { id: 'show1', content: 'How I Met Your Mother' },
+          { id: 'show2', content: 'Attack on Titan' },
+          { id: 'show3', content: 'Steins Gate' },
+        ],
       },
 
       'column3': {
         id: 'column3',
         title: 'Games',
-        itemIds: ['game1', 'game2', 'game3', 'game4'],
-        items: {
-          'game1': { id: 'game1', content: 'Cyberpunk' },
-          'game2': { id: 'game2', content: 'Death Stranding' },
-          'game3': { id: 'game3', content: 'Spiderman 2' },
-          'game4': { id: 'game4', content: 'God of War' },
-        },
+        items: [
+          { id: 'game1', content: 'Cyberpunk' },
+          { id: 'game2', content: 'Death Stranding' },
+          { id: 'game3', content: 'Spiderman 2' },
+          { id: 'game4', content: 'God of War' },
+        ],
       },
     },
-  
-  // Facilitate reordering of the columns
-  columnOrder: ['column1', 'column2', 'column3'],
 };
   
   export default initialData;
