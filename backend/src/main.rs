4cbebd06532bdc324358f6807a936ebf62cdfcb7
Anu <anu@netskrt.io>
2023-11-02 00:01:23 -0700

    added rocket

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,22 @@
+
+#![feature(proc_macro_hygiene, decl_macro)]
+
+#[macro_use] extern crate rocket;
+
+
+
+struct ListItem {
+    id: u64,
+    title: String,
+    order_key: String,
+}
+
+
+#[get("/data")]
+fn get_initial_data() -> String {
+    String::from("Test")
+}
+
 fn main() {
-    println!("Hello, world!");
+    rocket::ignite().mount("/", routes![get_initial_data]).launch();
 }
