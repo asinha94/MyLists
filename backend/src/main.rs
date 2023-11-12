5e51c2a3a89ccf8b4f7a5a855afd0945cf66a90d
Anu <anu@netskrt.io>
2023-11-12 03:28:36 -0800

    made lots of changes

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -7,25 +7,33 @@ use actix_cors::Cors;
 use serde::{Deserialize, Serialize};
 use serde_json;
 use sql::get_postgres_connect_uri;
-use sqlx::{Connection, PgConnection, Row, postgres::PgRow};
+use sqlx::{Connection, PgConnection};
 
 const HOST: &str = "127.0.0.1";
 const PORT: u16 = 8000;
 
+#[derive(Serialize, Deserialize)]
+#[derive(sqlx::FromRow)]
+struct DBItem {
+    id: i32,
+    category_title: String,
+    title: String,
+    order_key: String
+}
 
 #[derive(Serialize, Deserialize)]
-struct ListItem {
+struct UIItem {
     id: String,
     content: String,
     order_key: String,
 }
 
-impl ListItem {
-    fn new(id: u64, content: &str, order_key: &str) -> ListItem {
-        ListItem {
-            id: id.to_string(),
-            content: String::from(content),
-            order_key: String::from(order_key)
+impl UIItem {
+    fn new(item: &DBItem) -> UIItem {
+        UIItem {
+            id: item.id.to_string(),
+            content: item.title.clone(),
+            order_key: item.order_key.clone(),
         }
     }
 }
@@ -35,80 +43,47 @@ impl ListItem {
 struct Column {
     id: String,
     title: String,
-    items: Vec<ListItem>
+    items: Vec<UIItem>
 }
 
-
-#[derive(Serialize, Deserialize)]
-struct DBColumn {
-    category: String,
-}
-
-#[get("/categories")]
+#[get("/data")]
 async fn get_sql_categories() -> impl Responder {
 
     let connuri = get_postgres_connect_uri();
     let mut conn = PgConnection::connect(&connuri).await.unwrap();
 
-    let qry = sqlx::query("SELECT category_title from categories");
-    let rows = qry.map(|row: PgRow| DBColumn {
-        category: row.get("category_title"),
-    })
-    .fetch_all(&mut conn).await.unwrap();
-        
-    serde_json::to_string(&rows).unwrap()
-}
+    let items = sqlx::query_as::<_, DBItem>("
+        SELECT i.id, c.category_title, i.title, i.order_key
+        FROM items i
+        JOIN categories c ON c.id = i.category_id")
+        .fetch_all(&mut conn)
+        .await
+        .unwrap();
 
-
-#[get("/data")]
-async fn get_initial_data() -> impl Responder {
     let mut data = HashMap::new();
 
-    let movies = Column {
-        id: String::from("column1"),
-        title: String::from("Movies"),
-        items: vec![
-            ListItem::new(1, "Intersteller", "movie1"),
-            ListItem::new(2, "Inception", "movie2"),
-            ListItem::new(3, "Tenet", "movie3"),
-            ListItem::new(4, "Memento", "movie4"),
-        ]
-    };
+    for item in items {
+        // Lookup or insert
+        let mut category_list = data.entry(item.category_title.clone())
+            .or_insert(Column {
+                id: item.category_title.clone(),
+                title: item.category_title.clone(),
+                items: Vec::new(),
+            });
 
-    let shows = Column {
-        id: String::from("column2"),
-        title: String::from("TV Shows"),
-        items: vec![
-            ListItem::new(1, "How I Met Your Mother", "show1"),
-            ListItem::new(2, "Attack on Titan", "show2"),
-            ListItem::new(3, "Steins Gate", "show3"),
-        ]
-    };
-
-    let games = Column {
-        id: String::from("column3"),
-        title: String::from("Games"),
-        items: vec![
-            ListItem::new(1, "Cyberpunk 2077", "game1"),
-            ListItem::new(2, "Death Stranding", "game2"),
-            ListItem::new(3, "Spiderman 2", "game3"),
-            ListItem::new(4, "God of War", "game4"),
-        ]
-    };
-
-    data.insert(movies.title.clone(), movies);
-    data.insert(shows.title.clone(), shows);
-    data.insert(games.title.clone(), games);
-
+        category_list.items.push(UIItem::new(&item));
+    }
+    
     serde_json::to_string(&data).unwrap()
 }
+
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()>{
 
     HttpServer::new(|| {
         App::new()
-        .service(get_initial_data)
+        //.service(get_initial_data)
         .service(get_sql_categories)
         .wrap(Cors::permissive())
     })
--- backend/src/utils/parse.rs
+++ backend/src/utils/parse.rs
@@ -3,16 +3,12 @@ mod sql;
 
 use std::fs;
 use std::collections::HashMap;
-use sqlx::{Connection, PgConnection, Row, postgres::PgRow};
+use sqlx::{Connection, PgConnection};
 use tokio;
 
 
 const CATEGORIES_PATH: &str = "src/utils/categories.csv";
 const ITEMS_PATH: &str = "src/utils/items.csv";
-
-struct DBColumn {
-    category: String,
-}
 
 async fn insert_categories() {
     let err_string = format!("Failed to read {CATEGORIES_PATH}");
@@ -35,46 +31,91 @@ async fn insert_categories() {
 }
 
 
-async fn insert_items() {
-    let mut categories = HashMap::new();
-
-    let connuri = sql::get_postgres_connect_uri();
-    let mut conn = PgConnection::connect(&connuri).await.unwrap();
-    
-    let qry = sqlx::query("SELECT category_title from categories");
-    let categories_rows: Vec<DBColumn> = qry.map(|row: PgRow| DBColumn {
-        category: row.get("category_title"),
-    })
-    .fetch_all(&mut conn).await.unwrap();
-
-    for category in categories_rows {
-        let vecs: Vec<String> = Vec::new();
-        categories.insert(category.category, vecs);
-    }
-
+async fn insert_items() {    
     let err_string = format!("Failed to read {ITEMS_PATH}");
     let f = fs::read_to_string(ITEMS_PATH).expect(&err_string);
 
+    let mut items_by_category = HashMap::new();
 
-
+    let mut count: i32 = 0;
     for line in f.lines() {
-        let qry = "
-        INSERT INTO items (title, ordering_key) VALUES ($1, $2)
-        ON CONFLICT DO NOTHING";
-        sqlx::query(qry)
-            .bind(line)
-            .execute(&mut conn)
-            .await
-            .unwrap();
+        let ls: Vec<&str> = line.split(",").collect();
+        
+        if ls.len() != 2 {
+            println!("Malformed CSV line: {line}");
+            continue;
+        }
+
+        let category = ls[0];
+        let name = ls[1];
+
+        // Insert new vector if not available
+        let category_items =  items_by_category.entry(String::from(category))
+            .or_insert(Vec::new());
+
+        category_items.push(String::from(name));
+        count += 1;
     }
-    
-    
-
 
     
 
+    let connuri = sql::get_postgres_connect_uri();
+    let mut conn = PgConnection::connect(&connuri).await.unwrap();
+
+    let qry = "
+    INSERT INTO items (category_id, title, order_key)
+    SELECT id, $1, $2 FROM categories
+    WHERE category_title = $3
+    ON CONFLICT DO NOTHING";
+
+
+    // Create the key arrays. Start at b
+    const BASE: u8 = 12;
+    let key_width = count.ilog(12) + 1;
+    println!("Count is {key_width}");
+    let mut chars: Vec<u8> = Vec::new();
+    for i in 0..key_width {
+        chars.push(0);
+    }
+
+
+    for (category, items) in &items_by_category {
+
+        for item in items.iter() {
+            let mut key = String::from("key");
+            for chr in &chars {
+                let ascii: u8 = 98 + (chr * 2);
+                key.push(ascii as char);
+            }
+            increment_key_array(&mut chars, BASE);
+
+            sqlx::query(qry)
+                .bind(item)
+                .bind(&key)
+                .bind(category)
+                .execute(&mut conn)
+                .await
+                .unwrap();
+        }
+
+    }
 
 }
+
+fn increment_key_array(key: &mut Vec<u8>, max_value: u8) {
+    for i in (0..key.len()).rev() {
+        let val = key[i] + 1;
+        key[i] = val;
+
+        if val < max_value {
+            return;
+        }
+
+        key[i] = 0;
+
+    }
+}  
+
 
 #[tokio::main]
 async fn main() {
