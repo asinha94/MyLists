c8ce1c59ee5945fa56616e069540fdc2060037e8
Anu <anu@netskrt.io>
2023-11-30 02:14:10 -0800

    added build script

---
+++ build_n_deploy.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Fail on errors
+set -e
+
+# make sure latest code is installed
+git checkout master
+git pull
+
+# Build the backend service
+pushd backend
+cargo build --release
+sudo service backend restart
+popd
+
+
+# Build the frontend
+pushd frontend
+npm run build
+sudo service nginx restart
+popd
